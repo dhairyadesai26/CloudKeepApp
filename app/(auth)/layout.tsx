7c8c005a19b73706d3d9a69568e62361dbc3f0bf
dhairya833 <205036157+dhairyadesai26@users.noreply.github.com>
2025-07-31 19:23:02 +0530

    update

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -7,7 +7,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
       <section className="  hidden w-1/2 items-center justify-center bg-brand p-10 lg:flex xl:w-2/5">
         <div className="flex max-h-[800px] max-w-[430px] flex-col justify-center space-y-12">
           <Image
-  src="https://sdmntprwestus2.oaiusercontent.com/files/00000000-33c0-61f8-bca8-8abe1179a9e9/raw?se=2025-07-31T11%3A12%3A41Z&sp=r&sv=2024-08-04&sr=b&scid=9fe8d353-8697-56c9-ae7e-b16cc6489c2e&skoid=04233560-0ad7-493e-8bf0-1347c317d021&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2025-07-30T22%3A56%3A04Z&ske=2025-07-31T22%3A56%3A04Z&sks=b&skv=2024-08-04&sig=ndA%2BKDj7n5YByPeOJqPeWjCxuYEcbmiIPFaWGdyDjmE%3D"
+  src="/assets/ChatGPT Image Jul 31, 2025, 07_05_03 PM.png"
   alt="CloudKeep logo"
   width={300}
   height={300}
